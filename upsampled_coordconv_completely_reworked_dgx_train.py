751a28384fa8438f213f6eccf9dee7e733e631a7
Pedro Borges <p.borges.17@ucl.ac.uk>
2022-06-27 14:53:06 +0100

    Adjusting save logic

--- upsampled_coordconv_completely_reworked_dgx_train.py
+++ upsampled_coordconv_completely_reworked_dgx_train.py
@@ -1063,10 +1063,12 @@ if __name__ == '__main__':
                         if some_iter % 200 == 0:
                             save_img(seg_fake_A[:, 1, ...].squeeze().cpu().detach().numpy(),
                                      train_sample[0]['image_meta_dict']['affine'][0, ...],
-                                     os.path.join(FIG_DIR, f"seg_fake_A_iter_{some_iter}.nii.gz"))
+                                     os.path.join(FIG_DIR, f"seg_fake_A_iter_{some_iter}.nii.gz"),
+                                     overwrite=True)
                             save_img(real_B.squeeze().cpu().detach().numpy(),
                                      train_sample[0]['image_meta_dict']['affine'][0, ...],
-                                     os.path.join(FIG_DIR, f"seg_real_B_iter_{some_iter}.nii.gz"))
+                                     os.path.join(FIG_DIR, f"seg_real_B_iter_{some_iter}.nii.gz"),
+                                     overwrite=True)
                         del seg_fake_A, slog_fake_A
 
                     # Total loss
@@ -1417,10 +1419,12 @@ if __name__ == '__main__':
                                 if val_iter % 200 == 0:
                                     save_img(val_seg_fake_A[:, 1, ...].squeeze().cpu().detach().numpy(),
                                              val_affine,
-                                             os.path.join(FIG_DIR, f"val_seg_fake_A_iter_{some_iter}.nii.gz"))
+                                             os.path.join(FIG_DIR, f"val_seg_fake_A_iter_{some_iter}.nii.gz"),
+                                             overwrite=True)
                                     save_img(val_real_B.squeeze().cpu().detach().numpy(),
                                              val_affine,
-                                             os.path.join(FIG_DIR, f"val_real_B_iter_{some_iter}.nii.gz"))
+                                             os.path.join(FIG_DIR, f"val_real_B_iter_{some_iter}.nii.gz"),
+                                             overwrite=True)
                                 del val_seg_fake_A, val_slog_fake_A
 
                             if opt.perceptual and not opt.msssim:
